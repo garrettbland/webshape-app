43b77ae37091447c1588d979d88e27a763efe581
Garrett Bland <gmorganbland@gmail.com>
2022-07-22 09:26:47 -0500

    feat: more setting up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './style/tailwind.css'
 
 export const App = () => {
-    return <h1 className="text-8xl font-bold">Hello world! Here are changes</h1>
+    return <h1 className="text-8xl font-bold bg-blue-300">Hello world! Here are changes</h1>
 }
